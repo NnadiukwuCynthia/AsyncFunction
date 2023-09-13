0d9829856b0fd6fe7a93b3dd268f7b45fe256aa7
Cynthia Nnadiuwku <cynthiaamaran@gmail.com>
2023-09-13 16:33:04 +0100

    task: confirmed api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-// import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
-function App() {
-  // const [count, setCount] = useState(0)
+let API = 'https://fakestoreapi.com/products'
 
+function App () {
+  const [title, setTitle] = useState([])
+
+  useEffect(() => {
+  const fetchData = async () => {
+    const response = await axios.get(`${API}/1`)
+    console.log(response);
+    setTitle(response.data.title)
+  }
+  fetchData()
+
+},[])
   return (
     <>
-
+      <p>{title}</p>
         
     </>
   )
