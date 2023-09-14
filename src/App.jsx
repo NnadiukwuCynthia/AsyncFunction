2cf7ab3d18e02025c7f1db8a450ed33065e8d06e
Cynthia Nnadiuwku <cynthiaamaran@gmail.com>
2023-09-14 15:56:35 +0100

    task: modified code

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,27 @@ let API = 'https://fakestoreapi.com/products'
 
 function App () {
   const [title, setTitle] = useState([])
+  const [description, setDescription] = useState([])
+  const [image, setImage] = useState([])
 
   useEffect(() => {
   const fetchData = async () => {
     const response = await axios.get(`${API}/1`)
     console.log(response);
     setTitle(response.data.title)
+    setDescription(response.data.description)
+    setImage(response.data.image)
   }
   fetchData()
 
 },[])
   return (
     <>
-      <p>{title}</p>
+    <main className='eachItem'>
+      <img src={image} alt='' className='itemImage'/>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </main>
         
     </>
   )
