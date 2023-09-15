b95ec371cc99908fcbc56965598b81eba652fa05
Cynthia Nnadiuwku <cynthiaamaran@gmail.com>
2023-09-15 11:41:51 +0100

    Modified app

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,51 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import axios from 'axios'
 import './App.css'
-
-let API = 'https://fakestoreapi.com/products'
 
 function App () {
   const [number, setNumber]= useState(1)
   const [title, setTitle] = useState([])
-  const [description, setDescription] = useState([])
   const [image, setImage] = useState([])
-
-  useEffect(() => {
-  const fetchData = async () => {
-    const response = await axios.get(`${API}/1`)
-    console.log(response);
+  const [description, setDescription] = useState([])
+  
+  const getStoreData = async () => {
+    const response = await axios.get(`https://fakestoreapi.com/products/${number}`)
     setTitle(response.data.title)
-    setDescription(response.data.description)
     setImage(response.data.image)
+    setDescription(response.data.description)
+    console.log(response);
   }
-  fetchData()
-
-},[])
+  getStoreData()
   return (
     <>
     <main className='eachItem'>
-      <select value={number}>
+      <select value={number} onChange={e => (setNumber(e.target.value))}>
         <option value='1'>1</option>
         <option value='2'>2</option>
         <option value='3'>3</option>
         <option value='4'>4</option>
         <option value='5'>5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+        <option value="13">13</option>
+        <option value="14">14</option>
+        <option value="15">15</option>
       </select>
-      <section>
-      <img src={image} alt='' className='itemImage'/>
-        <h2>{title}</h2>
+       <section className='section'>
+       <div className='header'>
+       <h2> Product {number} is listed</h2>
+        <h1>{title}</h1>
+       </div>
+      <div className='product_container'>
+        <img src={image} alt='' className='itemImage'/>
         <p>{description}</p>
-      </section>
+      </div>
+       </section>
     </main>
         
     </>
